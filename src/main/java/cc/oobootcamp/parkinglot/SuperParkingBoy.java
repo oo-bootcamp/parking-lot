b21fc0739e5f3bcef6bcaa62ca80e13d0b134436
Yuan Shenjian <sjyuan@thoughtworks.com>
2019-07-14 22:49:49 +0800

    Rename variable
    Signed-off-by: Yuan Shenjian <sjyuan@thoughtworks.com>

--- src/main/java/cc/oobootcamp/parkinglot/SuperParkingBoy.java
+++ src/main/java/cc/oobootcamp/parkinglot/SuperParkingBoy.java
@@ -14,7 +14,7 @@ public class SuperParkingBoy extends ParkingBoy {
     public Ticket park(Car car) {
         return parkingLots.stream()
                 .max(Comparator.comparingDouble(ParkingLot::vacancyRate))
-                .filter(parkingLot1 -> !parkingLot1.isFull())
+                .filter(parkingLot -> !parkingLot.isFull())
                 .orElseThrow(ParkingLotFullException::new).park(car);
     }
 
